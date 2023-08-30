8711b0c9a19bafdc0658943032cbe735d278cde2
predrag jandric <predrag.jandric.bg@gmail.com>
2023-08-30 21:22:37 +0200

    initial project setup plus navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavbarComp from './components/NavbarComp';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+      <Container>
+      <NavbarComp>
+
+      </NavbarComp>
+      </Container>
+  );
+}
+
+export default App;
