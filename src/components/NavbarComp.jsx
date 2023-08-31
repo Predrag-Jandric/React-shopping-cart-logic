0159b70b494bb50664c99d893d5ab15bead4362a
predrag jandric <predrag.jandric.bg@gmail.com>
2023-08-31 23:57:38 +0200

    more logic but there is a problem with cart button displaying total items in cart

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Container, Navbar, Modal } from 'react-bootstrap'
+import { CartContext } from '../CartContext'
 
 function NavbarComp() {
 
+  const cart = useContext(CartContext)
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
 
+  const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
+console.log(productsCount);
   return (
     <>
       <Navbar expand="sm">
         <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button onClick={handleShow}>Cart 0 Items</Button>
+          <Button onClick={handleShow}>Cart has ({productsCount} Items)</Button>
         </Navbar.Collapse>
       </Navbar>
       <Modal show={show} onHide={handleClose}>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { Card, Button, Form, Row, Col } from 'react-bootstrap'
 import { CartContext } from '../CartContext'
 
@@ -6,20 +6,36 @@ import { CartContext } from '../CartContext'
 // props.product is the product we are selling
 function ProductCard(props) {
 
-        const product = props.product
-        const cart = useContext(CartContext)
-        const productQuantity = cart.getProductQuantity(product.id)
-        console.log([productQuantity]);
+    const product = props.product
+    const cart = useContext(CartContext)
+    const productQuantity = cart.getProductQuantity(product.id)
 
-        return (
-            <Card>
-                <Card.Body>
-                    <Card.Title>{product.title}</Card.Title>
-                    <Card.Text>€{product.price}</Card.Text>
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>€{product.price}</Card.Text>
+                {productQuantity > 0 ?
+                    <>
+                    <Form as={Row}>
+                        <Form.Label column="true" sm="6">
+                            In Cart: {productQuantity}
+                        </Form.Label>
+                        <Col sm="6">
+                            <Button onClick={() => cart.addOneToCart(product.id)} sm="6" className='mx-2'>+</Button>
+                            <Button onClick={() => cart.removeOneFromCart(product.id)} sm="6" className='mx-2'>-</Button>
+                        </Col>
+                    </Form>
+                    <Button onClick={() => cart.deleteFromCart(product.id)} variant='danger' className='my-2'>Remove from Cart</Button>
+                    </>
+                    :
                     <Button cariant="primary" onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
-                </Card.Body>
-            </Card>
-        )
-    }
+                }
+
+            </Card.Body>
+        </Card>
+    )
+}
 
 export default ProductCard
